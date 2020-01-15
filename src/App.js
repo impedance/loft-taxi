12e328b3ff6be42d35a36ee939f0d788e681e4da
impedance <2275399@gmail.com>
2020-01-15 12:49:15 +0300

    create samples for components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './components/header';
+import { Profile } from './components/profile';
+import { Login } from './components/login';
+import { Map } from './components/map';
+import { Registration } from './components/registration';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Profile />
+      <Map />
+      <Login />
+      <Registration />
+    </>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <>
+      <h1>Header</h1>
+      <button>Карта</button>
+      <button>Профиль</button>
+      <button>Логин</button>
+    </>
+  )
+}
