6faa5d4b2d28ff39050d32f4c64bebc2e687fc0c
impedance <2275399@gmail.com>
2020-01-20 16:52:23 +0300

    use hooks for state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header  from './components/Header';
 import Profile from './components/Profile';
 import Map  from './components/Map/Map';
@@ -7,35 +7,31 @@ import SignUp from './components/SignUp';
 
 const paths = ['login', 'profile', 'map', 'signup'];
 
-class App extends React.Component {
-  state = {
-    currentPath: 'signup'
-  };
-  onPathChange = (path: string) => {
-    this.setState({currentPath: path})
+const App = () => {
+  const [currentPath, setCurrentPath] = useState('login');
+
+  const onPathChange = (path: string) => {
+    setCurrentPath(path);
   }
-  renderCurrentComponent = (path: string) => {
+  const renderCurrentComponent = (path: string) => {
     switch (path) {
       case 'profile':
         return <Profile />;
       case 'map':
         return <Map />;
       case 'signup':
-        return <SignUp onPathChange={this.onPathChange}/>;
+        return <SignUp onPathChange={onPathChange}/>;
       default:
         return <Signin />;
     }
   }
-  render() {
-    const { currentPath } = this.state;
-    return (
-      <div>
-        <Header 
-          paths={paths} onPathChange={this.onPathChange} ></Header>
-          {this.renderCurrentComponent(currentPath)}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header 
+        paths={paths} onPathChange={onPathChange} ></Header>
+        {renderCurrentComponent(currentPath)}
+    </div>
+  );
 }
 
 export default App;
