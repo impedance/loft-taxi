8026aeb9d93f2008af79bdcc9e0134574072b194
impedance <2275399@gmail.com>
2020-01-26 17:47:41 +0300

    map added

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,51 +1,41 @@
 import React from 'react';
-// import mapboxgl from 
+import mapboxgl from 'mapbox-gl';
 
-export interface MapProps {
-  
-}
+export interface MapProps {}
  
-export interface MapState {
-  
-}
+export interface MapState {}
+
+const accessToken: string = 'pk.eyJ1IjoiaW1wZWRhbmNlIiwiYSI6ImNrNXR1OHA1YTB6bHEzbG1sOGlvczllbGQifQ.x4hErx7q1XqH9qHfrTFwJw';
+const coordinates: [number, number] = [-87.661557, 41.893748];
+const zoom: number = 10.7;
+const style: string = 'mapbox://styles/mapbox/streets-v9';
  
 class Map extends React.Component<MapProps, MapState> {
+  map: mapboxgl.Map | any;
+  mapContainer: any;
 
-  // componentDidMount() {
-  //   this.map = new mapboxgl.Map({
-  //     container: this.mapContainer,
-  //     style: 'mapbox://styles/mapbox/streets-v9'
-  //   });
-  // }
-  // componentWillUnmount() {
-  //   this.map.remove();
-  // }
-
-  
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: style,
+      center: coordinates,
+      zoom: zoom,
+      accessToken: accessToken,
+    });
+  }
+  componentWillUnmount() {
+    this.map.remove();
+  }
 
   render() {
-    const style = {
+    const style: object = {
       position: 'absolute',
       top: 0,
       bottom: 0,
       width: '100%'
     };
 
-    return (
-      <div id='map'></div>
-        <script>
-        mapboxgl.accessToken = 'pk.eyJ1IjoiaW1wZWRhbmNlIiwiYSI6ImNrNXR1OHA1YTB6bHEzbG1sOGlvczllbGQifQ.x4hErx7q1XqH9qHfrTFwJw'
-        var map = new mapboxgl.Map({
-          container: 'map',
-          style: 'mapbox://styles/mapbox/streets-v9', 
-          center: [-87.661557, 41.893748],
-          zoom: 10.7
-        });
-        // code from the next step will go here
-        </script>
-    )
-
-    // return <div style={style} ref={el => this.mapContainer = el} />;
+    return <div style={style} ref={el => this.mapContainer = el} />;
   }
 }
  
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { theme } from "loft-taxi-mui-theme";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>    
+    <App />
+  </MuiThemeProvider>, document.getElementById('root')
+);
